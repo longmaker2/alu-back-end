4419b15454b3a3a71cabb703d2bca1c9e5706d6e
longmaker2 <l.deng@alustudent.com>
2023-02-22 02:12:27 +0200

    files updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress
+    a Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress
 """
 import json
 import requests
